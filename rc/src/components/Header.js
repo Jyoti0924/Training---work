ce942b2e4e4f4cad3d44ad3efbaaa6598ef926be
Jyoti <125297747+Jyoti0924@users.noreply.github.com>
2023-08-08 17:36:53 +0530

    logo changes

--- rc/src/components/Header.js
+++ rc/src/components/Header.js
@@ -6,7 +6,7 @@ const Header = () => {
     <div>
     <nav class="navbar navbar-expand-lg navbar-light bg-light">
     <div class="container-fluid">
-     <img src='images/img1.png' alt='logo' className='logo'></img>
+     <img src='images/img1.jpeg' alt='logo' className='logo'></img>
       <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
         <span class="navbar-toggler-icon"></span>
       </button>
--- rc/src/components/Home.js
+++ rc/src/components/Home.js
@@ -1,22 +1,53 @@
 import React from 'react'
+import { useState } from 'react'
+import { useNavigate ,useLocation } from 'react-router-dom';
 const Home = () => {
+  const [state, setState]=useState({
+    firstname:"",
+    lastname:"",
+    email:""
+  });
+  const [show ,setShow]=useState(false)
+  const handleChange = (event)=>{
+    
+      const {name,value} = event.target;
+      setState((prevProps)=>({
+        
+      ...prevProps,  //...spread operator 
+        [name]:value
+}));
+  };
+  const handleSubmit = (event) => {
+    setShow(true)
+    event.preventDefault();
+    console.log(state);
+  };
+  console.log(show)
   return (
-    <div>
-     <>
-     <div>
+    <>
+    <div className='form'>
+    <div > 
+     <form onSubmit={handleSubmit}>
      <label className='fname' >Enter Name</label>
-     <input type='text' autoComplete='off' placeholder='Enter first name'></input>
+     <input className='ip' type='text' name="firstname" value={state.firstname} onChange={handleChange} autoComplete='off' placeholder='Enter first name'/>
+  
      <label className='fname' >Enter Last Name</label>
-     <input type='text' autoComplete='off' placeholder='Enter last name'></input>
+     <input className='ip' type='text' name="lastname" value={state.lastname}  onChange={handleChange} autoComplete='off' placeholder='Enter last name'/>
      <label className='fname' >Enter Gmail </label>
-     <input type='text' autoComplete='off' placeholder='Enter your gmail'></input>
+     <input className='ip' type='email' name="email" value={state.email} onChange={handleChange} autoComplete='off' placeholder='Enter your gmail'/>
+
+    <div>
+     <button  type='submit'className='bname' >submit</button>
+     </div>
+     </form>
+    {show?<h3>{state.firstname},{state.lastname},{state.email}</h3>:''}
      
      </div>
-     <button className='bname' type='submit' >submit</button>
+     </div>
+     
      </>
-    </div>
-  )
-}
+  );
+};
 
 export default Home
 
