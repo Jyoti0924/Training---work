0f137ed20bdd69cfe11c621e8c358ab3d5bba9b3
Jyoti <125297747+Jyoti0924@users.noreply.github.com>
2023-08-09 17:29:32 +0530

    navigation done

--- rc/src/components/About.jsx
+++ rc/src/components/About.jsx
@@ -1,13 +1,37 @@
 import React from 'react'
-
+import { useLocation,useNavigate  } from 'react-router-dom'
 const About = () => {
+  const Location = useLocation();
+  const navigate = useNavigate();
+  const handleClick =() =>{
+    navigate('/')
+  }
   return (
   <>
-  <h2>Welcome to About page</h2>
+
+  <div className='about'>
+  <form>
+  <div className='about1'>
+  
+  <p className='p'>Firstname Received : </p>{Location.state ==null?'':<h5 >{Location.state.h.firstname}</h5>}
+  </div>
+  <div className='about2'>
+  <p className='p'>Lastname Received : </p>{Location.state ==null?'':<h5 >{Location.state.h.lastname}</h5>}
+  </div>
+  <div className='about3'>
+  <p className='p'>Email Received : </p>{Location.state ==null?'':<h5>{Location.state.h.email}</h5>}
+  </div>
+  </form>
+  </div>
+  <div className='btn'>
+  <button onClick={()=>handleClick()}>goBack</button>
+  </div>
   </>
   )
 }
+export default About;
 
-export default About
+
+
 
 
--- rc/src/components/Header.js
+++ rc/src/components/Header.js
@@ -4,7 +4,7 @@ import { Link } from 'react-router-dom'
 const Header = () => {
   return (
     <div>
-    <nav class="navbar navbar-expand-lg navbar-light bg-light">
+    <nav class="navbar navbar-expand-lg navbar-light bg-light ">
     <div class="container-fluid">
      <img src='images/img1.jpeg' alt='logo' className='logo'></img>
       <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
